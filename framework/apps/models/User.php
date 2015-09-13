b7f9b173e0093552d41adf295eacb080c58c174c
amydu703 <jack6q@gmail.com>
2015-09-13 13:20:43 +0800

    修改用户昵称和头像

--- framework/apps/models/User.php
+++ framework/apps/models/User.php
@@ -9,4 +9,26 @@ class User extends Swoole\Model
      * @var string
      */
     public $table = 'users';
+
+
+    public function getUserInfo(){
+        $sql = "select * from users  where id=4";
+        $data = $this->db->query($sql)->fetch();
+        return $data;
+
+    }
+
+
+    public function toUpdateAvatar($id,$data,$where){
+        $result = parent::set($id,$data,$where);
+
+        if (false === $result) {
+            return ['code' => 1, 'data' => '', 'msg' => 'fail'];
+        } else {
+            return ['code' => 0, 'data' => '', 'msg' => 'success'];
+        }
+
+
+    }
+
 }
